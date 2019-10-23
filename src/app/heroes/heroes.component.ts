cada785f9253adf40f992aa3a8e9cfc7f6c5604c
joshtom <olajidejoshua4real@gmail.com>
2019-10-23 02:16:48 +0100

    Added onSelect method which  which assigns the clicked hero from the template to the component's selectedHero.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -13,6 +13,10 @@ export class HeroesComponent implements OnInit {
     name: 'Windstorm'
   };
   heroes = HEROES;
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
   constructor() { }
 
   ngOnInit() {
