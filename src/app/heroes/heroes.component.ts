2a495994cfad8ca438e8faca9a42153ab9ad8aa2
joshtom <olajidejoshua4real@gmail.com>
2019-10-23 03:51:06 +0100

    Created a function to retrieve the heroes from the service.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -12,13 +13,17 @@ export class HeroesComponent implements OnInit {
     id: 1,
     name: 'Windstorm'
   };
-  heroes = HEROES;
+  // heroes = HEROES;
+  heroes: Hero[];
+  getHeroes(): void{
+    this.heroes = this.heroService.getHeroes();
+  }
   // Add the following onSelect() method, which assigns the clicked hero from the template to the component's selectedHero.
   selectedHero: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
   }
