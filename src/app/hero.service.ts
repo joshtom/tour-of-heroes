cbee3327540bc2a58038639855b8778b0ac2822b
joshtom <olajidejoshua4real@gmail.com>
2019-10-26 17:49:44 +0100

    Fixed the heroes component not showing

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -9,10 +9,11 @@ import { MessageService } from './message.service';
   providedIn: 'root'
 })
 export class HeroService {
+  constructor(private messageService: MessageService) { }
   getHeroes(): Observable<Hero[]> {
     // TODO: send the message _after_ fetching the heroes
     this.messageService.add('HeroService: fetched heroes');
     return of(HEROES) ;
   }
-  constructor(private messageService: MessageService) { }
+  
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
 import { HeroService } from '../hero.service';
 
 @Component({
