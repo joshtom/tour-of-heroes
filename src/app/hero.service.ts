482951457d95601463170d2bb75852ab6b2e75ce
joshtom <olajidejoshua4real@gmail.com>
2019-10-26 17:19:11 +0100

    Message sent after fetching from the heroes component

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -10,7 +10,9 @@ import { MessageService } from './message.service';
 })
 export class HeroService {
   getHeroes(): Observable<Hero[]> {
+    // TODO: send the message _after_ fetching the heroes
+    this.messageService.add('HeroService: fetched heroes');
     return of(HEROES) ;
   }
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 }
