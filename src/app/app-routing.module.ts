46c1dc635e924301f48d84f12e71ab4fb9f7a89b
joshtom <olajidejoshua4real@gmail.com>
2019-10-29 17:24:30 +0100

    Fixed the routing bug

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ const routes: Routes = [
   {path: 'heroes', component: HeroesComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  { path: 'details/:id', component: HeroDetailComponent }
+  { path: 'detail/:id', component: HeroDetailComponent }
 ];
 
 @NgModule({
