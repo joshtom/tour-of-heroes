1e13d7acc869a2c87db2c02e6f30110aa4666bd2
joshtom <olajidejoshua4real@gmail.com>
2019-10-29 16:40:35 +0100

    Cleaned up the code on heroes.component file

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,24 +8,16 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
   heroes: Hero[];
-  getHeroes(): void{
-    this.heroService.getHeroes()
-    .subscribe(heroes => this.heroes = heroes);
-  }
-  // Add the following onSelect() method, which assigns the clicked hero from the template to the component's selectedHero.
-  selectedHero: Hero;
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
+
   constructor(private heroService: HeroService) { }
 
   ngOnInit() {
     this.getHeroes();
   }
 
+  getHeroes(): void {
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
+  }
 }
