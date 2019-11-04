641c4b941a2b95da82b222dcc01a881d52fd3aba
joshtom <olajidejoshua4real@gmail.com>
2019-11-04 06:53:32 +0100

    I added a save button with a click event binding that invokes a new component method named save()

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -23,12 +23,39 @@ export class HeroService {
     // TODO: send the message _after_ fetching the heroes
     return this.http.get<Hero[]>(this.heroesUrl)
     .pipe(
+      tap(_ => this.log('fetched heroes')),
       catchError(this.handleError<Hero[]>('getHeroes', []))
     );
   }
+        
+
+/** GET hero by id. Will 404 if id not found */
   getHero(id: number): Observable<Hero> {
     // TODO: send the message _after_ fetching the hero
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(HEROES.find(hero => hero.id === id));
+    const url = `${this.heroesUrl}/${id}`;
+  return this.http.get<Hero>(url).pipe(
+    tap(_ => this.log(`fetched hero id=${id}`)),
+    catchError(this.handleError<Hero>(`getHero id=${id}`))
+  );
   }
+
+  /**
+ * Handle Http operation that failed.
+ * Let the app continue.
+ * @param operation - name of the operation that failed
+ * @param result - optional value to return as the observable result
+ */
+private handleError<T> (operation = 'operation', result?: T) {
+  return (error: any): Observable<T> => {
+
+    // TODO: send the error to remote logging infrastructure
+    console.error(error); // log to console instead
+
+    // TODO: better job of transforming error for user consumption
+    this.log(`${operation} failed: ${error.message}`);
+
+    // Let the app keep running by returning an empty result.
+    return of(result as T);
+  };
+}
 }
