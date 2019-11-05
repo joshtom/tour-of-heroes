f21a96753e8033ab7d90118c71914b143613f024
joshtom <olajidejoshua4real@gmail.com>
2019-11-05 17:24:28 +0100

    Added the searchHeroes method

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -85,4 +85,15 @@ deleteHero (hero: Hero | number): Observable<Hero> {
     catchError(this.handleError<Hero>('deleteHero'))
   );
 }
+/* GET heroes whose name contains search term */
+searchHeroes(term: string): Observable<Hero[]> {
+  if (!term.trim()) {
+    // if not search term, return empty hero array.
+    return of([]);
+  }
+  return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
+    tap(_ => this.log(`found heroes matching "${term}"`)),
+    catchError(this.handleError<Hero[]>('searchHeroes', []))
+  );
+}
 }
