1ade9305f7cda7c466e845ca61924bbf4726eeb1
joshtom <olajidejoshua4real@gmail.com>
2019-11-05 17:02:10 +0100

    I just added a addHero method to hero.service method

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -68,4 +68,11 @@ updateHero (hero: Hero): Observable<any> {
     catchError(this.handleError<any>('updateHero'))
   );
 }
+/** POST: add a new hero to the server */
+addHero (hero: Hero): Observable<Hero> {
+  return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+    tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+    catchError(this.handleError<Hero>('addHero'))
+  );
+}
 }
